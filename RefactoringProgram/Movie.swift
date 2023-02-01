5781abe27a673296b15521d4c82a248cb575046d
miguel <gannasong@gmail.com>
2023-02-01 23:04:09 +0800

    extract function

--- RefactoringProgram/Movie.swift
+++ RefactoringProgram/Movie.swift
@@ -31,4 +31,28 @@ public class Movie {
     public func getTitle() -> String {
         return title
     }
+
+    public func getCharge(_ daysRented: Int) -> Double {
+        var result: Double = 0
+        
+        switch getPriceCode() {
+            case Movie.REGULAR:
+                result += 2
+                if daysRented > 2 {
+                    result += Double((daysRented - 2)) * 1.5
+                }
+
+            case Movie.NEW_RELEASE:
+                result += Double(daysRented * 3)
+            case Movie.CHILDRENS:
+                result += 1.5
+                if daysRented > 3 {
+                    result += Double((daysRented - 3)) * 1.5
+                }
+            default:
+                fatalError("❌ unexpected switch case")
+        }
+
+        return result
+    }
 }
